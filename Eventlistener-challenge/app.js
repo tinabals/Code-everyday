375424e348f33e0eb9b68253c1983f0c4f5b685e
Damilola Balogun <balogundamilola.f@gmail.com>
2022-08-23 19:49:12 +0100

    challenge week 2 for sheCodes

--- Eventlistener-challenge/app.js
+++ Eventlistener-challenge/app.js
@@ -2,11 +2,16 @@ function updateHeading(newHeading) {
   let heading = document.querySelector('h1');
   heading.innerHTML = newHeading;
 }
+function canApply() {
+  let age = prompt('How old are you?');
+  if (age >= 18) {
+    return age;
+  }
+}
 function applyNow() {
   let firstName = prompt('What is your first name?');
-  let age = prompt('How old are you?');
 
-  if (age >= 18) {
+  if (canApply()) {
     updateHeading(`Hi,  ${firstName}! Welcome to SheCodes 🙎‍♀️ `);
   } else {
     updateHeading(`Sorry, ${firstName}! You cannot join SheCodes. `);
