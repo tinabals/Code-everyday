bdc52099be3427e0e1a2a48c0d26ebb8b8576982
Damilola Balogun <balogundamilola.f@gmail.com>
2022-09-02 07:36:46 +0100

    Javascript Maths(Math.min.max.ceil) etc

---
+++ SheCodes Foundation/She Codes Plus/Week3/JS Maths/app.js
@@ -0,0 +1,21 @@
+let number = 2.5;
+console.log(number);
+number = Math.round(number);
+console.log(number);
+
+let floor = 2.3;
+console.log(floor);
+
+floor = Math.floor(floor);
+console.log(floor);
+
+let a = 2.4;
+let b = 17;
+
+console.log(Math.min(a, b));
+console.log(Math.max(a, b));
+
+console.log(Math.random());
+
+let ceil = 4.2;
+console.log(Math.ceil(ceil));
