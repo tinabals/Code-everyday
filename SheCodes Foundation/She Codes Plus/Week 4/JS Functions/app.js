d9d87e75baed84aabbcc2d05f3228c7d10eb2e07
Damilola Balogun <balogundamilola.f@gmail.com>
2022-09-06 13:53:31 +0100

    JS Functions

---
+++ SheCodes Foundation/She Codes Plus/Week 4/JS Functions/app.js
@@ -0,0 +1,18 @@
+// let temperature = 18;
+// if (temperature > 15) {
+//   alert('It is a warm day');
+// } else {
+//   alert('It is a cold day');
+// }
+
+// The problem with this is that this code isn't reusable so we have to introduce functions
+// a fnction name is a verb because it does an action
+function welcomeUser() {
+  let temperature = 18;
+  if (temperature > 15) {
+    alert('It is a warm day');
+  } else {
+    alert('It is a cold day');
+  }
+}
+welcomeUser();
