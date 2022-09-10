b5d687585903fb17171dffe831a8654ccba54c21
Damilola Balogun <balogundamilola.f@gmail.com>
2022-09-10 21:09:55 +0100

    JS Selectors Challenge

--- SheCodes Foundation/She Codes Plus/Week 4/JS Selectors/app.js
+++ SheCodes Foundation/She Codes Plus/Week 4/JS Selectors/app.js
@@ -1,2 +1,17 @@
-let headings = document.querySelector('h1');
-headings.innerHTML = 'Js Selectors For sure 😉';
+// Challenge 1
+// Log li with id special
+let idSpecial = document.querySelector('#special');
+console.log(idSpecial);
+
+// Challenge 2
+// Log all li with class of country
+let allClassCountry = document.querySelectorAll('.country');
+console.log(allClassCountry);
+
+// Challenge 3
+// Add class special to the li with id special
+idSpecial.classList.add('.special');
+// Challenge 4
+// In your code, replace the content of the element with id special
+// by your favorite country (other than Portugal of course)
+idSpecial.innerHTML = 'Nigeria';
