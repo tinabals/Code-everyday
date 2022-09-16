b9ce5e58bdbea53eeebf9a1c02baa8366ec21c1f
Damilola Balogun <balogundamilola.f@gmail.com>
2022-09-17 00:00:24 +0100

    geo

--- SheCodes Foundation/She Codes Plus/Week 5/Geolocation Api/app.js
+++ SheCodes Foundation/She Codes Plus/Week 5/Geolocation Api/app.js
@@ -1,5 +1,12 @@
 function showPosition(position) {
-  console.log(position);
+  console.log(position.coords.latitude);
+  console.log(position.coords.longtitude);
 }
 
 navigator.geolocation.getCurrentPosition(showPosition);
+let button = document.querySelector('button');
+button.addEventListener('click', showCurrentPositiom);
+
+function showCurrentPositiom() {
+  let h1 = doc;
+}
