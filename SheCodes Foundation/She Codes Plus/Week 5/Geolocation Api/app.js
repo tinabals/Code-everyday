5fb7bdcef32c8f02c86216c72065c7b8dcc74df8
Damilola Balogun <balogundamilola.f@gmail.com>
2022-09-18 22:52:43 +0100

    weather search engine

--- SheCodes Foundation/She Codes Plus/Week 5/Geolocation Api/app.js
+++ SheCodes Foundation/She Codes Plus/Week 5/Geolocation Api/app.js
@@ -1,12 +1,12 @@
 function showPosition(position) {
   console.log(position.coords.latitude);
   console.log(position.coords.longtitude);
+  function showCurrentPositiom() {
+    let h1 = document.querySelector('h1');
+    h1.innerHTML = position.coords.latitude;
+  }
+  button.addEventListener('click', showCurrentPositiom);
 }
 
 navigator.geolocation.getCurrentPosition(showPosition);
 let button = document.querySelector('button');
-// button.addEventListener('click', showCurrentPositiom);
-
-// function showCurrentPositiom() {
-//   let h1 = doc;
-// }
--- SheCodes Foundation/She Codes Plus/Week 5/week 5 pre project/app.js
+++ SheCodes Foundation/She Codes Plus/Week 5/week 5 pre project/app.js
@@ -1,3 +1,4 @@
+//getting the current date and time
 let now = new Date();
 let hour = now.getHours();
 let minutes = now.getMinutes();
@@ -19,6 +20,7 @@ if (minutes <= 9) {
   todays_date.innerHTML = `${day} ${hour}:${minutes}`;
 }
 
+//conversion to fahrenheit and viceversa
 let fahrenheit = document.querySelector('.fahrenheit');
 fahrenheit.addEventListener('click', convertToCelsius);
 let currentTemp = document.querySelector('#currentTemp');
@@ -35,32 +37,42 @@ function convertToFahrenheit(event) {
   let fahrenheitConversion = (currentTemp.innerHTML * 9) / 5 + 32;
   currentTemp.innerHTML = Math.round(fahrenheitConversion);
 }
+//getting the current weather function
+function getCurrentWeather(response) {
+  let responseData = response.data;
+  let description = responseData.weather[0].description;
+  let descriptionElement = document.querySelector('.description');
+  descriptionElement.innerHTML = description;
+  let temperature = responseData.main.temp;
+  let h1 = document.querySelector('#currentTemp');
+  h1.innerHTML = `${temperature}`;
+  let location = document.querySelector('.heading-two');
+  location.innerHTML = responseData.name;
+}
 
+//getting weather through current position
+let showPositionButton = document.querySelector('.geolocation');
+function showPosition(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  function showCurrentPosition() {
+    apiKey = '2718952144ed077c12e7c160fb6fc351';
+    apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric`;
+    axios.get(`${apiUrl}&appid=${apiKey}`).then(getCurrentWeather);
+  }
+  showPositionButton.addEventListener('click', showCurrentPosition);
+}
+navigator.geolocation.getCurrentPosition(showPosition);
+
+//getting weather through searching
 let search_form = document.querySelector('#search-form');
 function searchCity(event) {
   event.preventDefault();
   let search_input = document.querySelector('.search-input');
   let value = search_input.value;
-  let apiKey = 'c6afaa2c3ad098fb65625db7f7ba5be4';
+  let apiKey = '2718952144ed077c12e7c160fb6fc351';
   let city = value;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-  function getCurrentWeather(response) {
-    let responseData = response.data;
-    let description = responseData.weather[0].description;
-    let descriptionElement = document.querySelector('.description');
-    descriptionElement.innerHTML = description;
-    let temperature = responseData.main.temp;
-    let h1 = document.querySelector('#currentTemp');
-    w;
-    h1.innerHTML = `${temperature}`;
-  }
-  axios.get(apiUrl).then(getCurrentWeather);
-  let location = document.querySelector('.heading-two');
-  location.innerHTML = value.toUpperCase();
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(getCurrentWeather);
 }
 search_form.addEventListener('submit', searchCity);
-
-function showPosition(position) {
-  console.log(position);
-}
-navigator.geolocation.getCurrentPosition(showPosition);
